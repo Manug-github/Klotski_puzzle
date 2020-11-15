d862f4c76d2e68e90911b892f1fc2178edbedeb7
Manug-local <34220384+Manug-github@users.noreply.github.com>
2020-11-15 03:51:51 +0100

    Add move blocks 2x1, ERROR Error undefined, if you combine all movements.

--- Klotski.py
+++ Klotski.py
@@ -1,12 +1,19 @@
 # https://en.wikipedia.org/wiki/Klotski
-
+import sys 
+# the setrecursionlimit function is 
+# used to modify the default recursion 
+# limit set by python. Using this,  
+# we can increase the recursion limit 
+# to satisfy our needs 
+  
+sys.setrecursionlimit(10**6) 
 
 def is_solved(board):
     # if board[3][1] == 6:
     #     return True
     if not board:
         return False
-    if board[0][0] == 1:
+    if board[4][1] == 3:
         return True
     return False
 
@@ -14,6 +21,7 @@ def is_solved(board):
 def next_movement1(board,used,row,col):
 
     copy_board = [x[:] for x in board]
+    # Mode UP
     if row>0:
         if board[row-1][col]==0:
             board[row-1][col] = 1
@@ -23,16 +31,17 @@ def next_movement1(board,used,row,col):
                 used.add(res)
                 next_movement(board,used)
             board = [x[:] for x in copy_board]
+    # Mode DOWN
     if row<4:
         if board[row+1][col]==0:
-            board[row+1][col] = 1
             board[row][col] = 0
+            board[row+1][col] = 1
             res = tuple(map(tuple, board))
             if res not in used:
                 used.add(res)
                 next_movement(board,used)
             board = [x[:] for x in copy_board]
-
+    # Mode LEFT
     if col>0:
         if board[row][col-1]==0:
             board[row][col-1] = 1
@@ -42,10 +51,11 @@ def next_movement1(board,used,row,col):
                 used.add(res)
                 next_movement(board,used)
             board = [x[:] for x in copy_board]
+    # Mode RIGHT
     if col<3:
         if board[row][col+1]==0:
-            board[row][col+1] = 1
             board[row][col] = 0
+            board[row][col+1] = 1
             res = tuple(map(tuple, board))
             if res not in used:
                 used.add(res)
@@ -55,9 +65,115 @@ def next_movement1(board,used,row,col):
 
 
 def next_movement2(board,used,row,col):
+    
+    copy_board = [x[:] for x in board]
+    print(row,col)
+    # Mode UP
+    if row>0:
+        if board[row-1][col]==0:
+            board[row-1][col] = 2
+            board[row][col] = 3
+            board[row+1][col] = 0
+            res = tuple(map(tuple, board))
+            if res not in used:
+                used.add(res)
+                next_movement(board,used)
+            board = [x[:] for x in copy_board]
+    # Mode DOWN
+    if row<3:
+        if board[row+2][col]==0:
+            board[row][col] = 0
+            board[row+1][col] = 2
+            board[row+2][col] = 3
+            res = tuple(map(tuple, board))
+            if res not in used:
+                used.add(res)
+                next_movement(board,used)
+            board = [x[:] for x in copy_board]
+    #Move LEFT
+    if col>0:
+        if board[row][col-1]==0 and board[row+1][col-1]==0:
+            board[row][col-1] = 2
+            board[row+1][col-1] = 3
+            board[row][col] = 0
+            board[row+1][col] = 0
+            res = tuple(map(tuple, board))
+            if res not in used:
+                used.add(res)
+                next_movement(board,used)
+            board = [x[:] for x in copy_board]
+    #Move RIGHT
+    if col<3:
+        if board[row][col+1]==0 and board[row+1][col+1]==0:
+            board[row][col] = 0
+            board[row+1][col] = 0
+            board[row][col+1] = 2
+            board[row+1][col+1] = 3
+            res = tuple(map(tuple, board))
+            if res not in used:
+                used.add(res)
+                next_movement(board,used)
+            board = [x[:] for x in copy_board]
+    return
+
+
+def next_movement4(board,used,row,col):
+
+    copy_board = [x[:] for x in board]
+    # Move UP
+    if row>0:
+        if board[row-1][col]==0 and board[row-1][col+1]==0:
+            board[row-1][col] = 4
+            board[row-1][col+1] = 5
+            board[row][col] = 0
+            board[row][col+1] = 0
+            res = tuple(map(tuple, board))
+            if res not in used:
+                used.add(res)
+                next_movement(board,used)
+            board = [x[:] for x in copy_board]
+    # Move DOWN
+    if row<4:
+        if board[row+1][col]==0 and board[row+1][col+1]==0:
+            print("dowww")
+            board[row][col] = 0
+            board[row][col+1] = 0
+            board[row+1][col] = 4
+            board[row+1][col+1] = 5
+            res = tuple(map(tuple, board))
+            if res not in used:
+                used.add(res)
+                next_movement(board,used)
+            board = [x[:] for x in copy_board]
+    # Mode LEFT
+    if col>0 :
+        if board[row][col-1]==0:
+            board[row][col-1] = 4
+            board[row][col] = 5
+            board[row][col+1] = 0
+            res = tuple(map(tuple, board))
+            if res not in used:
+                used.add(res)
+                next_movement(board,used)
+            board = [x[:] for x in copy_board]
+    # Mode RIGHT
+    if col<2:
+        if board[row][col+2]==0:
+            board[row][col] = 0
+            board[row][col+1] = 4
+            board[row][col+2] = 5
+            res = tuple(map(tuple, board))
+            if res not in used:
+                used.add(res)
+                next_movement(board,used)
+            board = [x[:] for x in copy_board]
+    return
+
+
+def next_movement6(board,used,row,col):
     copy_board = [x[:] for x in board]
     if row>1:
-        if board[row-2][col]==0:
+        if board[row-1][col]==0 and board[row-1][col+1]==0:
             board[row-2][col] = 3
             board[row-1][col] = 2
             board[row][col] = 0
@@ -77,43 +193,15 @@ def next_movement2(board,used,row,col):
                 next_movement(board,used)
             board = [x[:] for x in copy_board]
 
-    if col>0 and row>0:
-        if board[row-1][col-1]==0 and board[row][col-1]==0:
-            board[row-1][col-1] = 3
-            board[row][col-1] = 2
-            board[row-1][col] = 0
-            board[row][col] = 0
-            res = tuple(map(tuple, board))
-            if res not in used:
-                used.add(res)
-                next_movement(board,used)
-            board = [x[:] for x in copy_board]
-    if col<3 and row>0:
-        if board[row-1][col+1]==0 and board[row][col+1]==0:
-            board[row-1][col+1] = 3
-            board[row][col+1] = 2
-            board[row-1][col] = 0
-            board[row][col] = 0
-            res = tuple(map(tuple, board))
-            if res not in used:
-                used.add(res)
-                next_movement(board,used)
-            board = [x[:] for x in copy_board]
-    return
-
-def next_movement4(board,used,row,col):
-    pass
-
-def next_movement6(board,used,row,col):
-    pass
 
 def next_movement(board,used):
     global sol
     print("------------------")
     for row in board:
         print(row)
-
+    print(len(used))
     if is_solved(board):
+        print("+++++++++++++")
         sol = [x[:] for x in board]
         return
 
@@ -125,8 +213,8 @@ def next_movement(board,used):
                 next_movement1(board,used,row,col)
             if board[row][col] == 2:
                 next_movement2(board,used,row,col)
-            # elif board[row][col] == 4:
-            #     next_movement4(board,used,row,col)
+            if board[row][col] == 4:
+                next_movement4(board,used,row,col)
             # elif board[row][col] == 6:
             #     next_movement6(board,used,row,col)
                 
@@ -134,23 +222,23 @@ def next_movement(board,used):
 def solve(board,used):
     
     while not is_solved(sol):
-        print(board)
+        # print(board)
         next_movement(board,used)
 
 
 board=[
-        [3,6,7,3],
-        [2,8,9,2],
-        [3,4,5,3],
-        [2,1,1,2],
+        [2,6,7,2],
+        [3,8,9,3],
+        [2,4,5,2],
+        [3,1,1,3],
         [1,0,0,1]
 ]
 
 board=[
-        [3,6,7,3],
-        [2,8,9,2],
+        [2,2,2,2],
+        [3,3,3,3],
+        [2,4,5,2],
         [3,1,1,3],
-        [2,0,0,2],
         [1,0,0,1]
 ]
 
